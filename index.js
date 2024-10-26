91b46c53e1ce3b17965028976822b84fafe18cd6
vintage-vibes <sajimusa5@gmail.com>
2024-10-26 18:35:30 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function toogleMenu(){
   const menu= document.querySelector(".menu-links")
   const icon =document.querySelector('.hamburger-icon')
+
+  
   menu.classList.toggle('open')
   icon.classList.toggle('open')
 }
